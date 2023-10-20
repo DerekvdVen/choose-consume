769d94b0829f65038859a60854ccc633d1c897b6
Derek van de Ven <derek.vandeven@wur.nl>
2023-10-20 13:28:10 +0200

    forgot to save practice app

--- service/practicy_kivy_camera_app.py
+++ service/practicy_kivy_camera_app.py
@@ -1,46 +1,50 @@
-import kivy
-kivy.require('2.2.1') # replace with your current kivy version !
-
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+from kivy.uix.camera import Camera
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.graphics.texture import Texture
 
-
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
-
+class CameraApp(App):
     def build(self):
-        return CameraClick()
+        self.layout = BoxLayout(orientation='vertical')
 
+        # Create a Camera widget
+        self.camera = Camera(resolution=(640, 480), play=True)
+        self.layout.add_widget(self.camera)
 
-TestCamera().run()
+        # Create a button to capture a picture
+        self.capture_button = Button(text="Capture")
+        self.capture_button.bind(on_press=self.capture)
+        self.layout.add_widget(self.capture_button)
+
+        return self.layout
+
+    def capture(self, instance):
+        # Define a callback function to capture the picture
+        def on_picture_taken(texture):
+            # Save the captured image
+            texture.save("captured_image.png")
+            popup.dismiss()
+            self.show_captured_image()
+
+        # Capture a picture and set the callback
+        texture = self.camera.texture
+        if texture:
+            popup = Popup(title='Capturing...', content=Label(text='Capturing image, please wait...'), auto_dismiss=False, size_hint=(None, None), size=(400, 200))
+            popup.open()
+            Clock.schedule_once(lambda dt: on_picture_taken(texture), 2)
+
+    def show_captured_image(self):
+        # Display the captured image
+        self.layout.clear_widgets()
+        image = Image(source='captured_image.png')
+        self.layout.add_widget(image)
+
+if __name__ == '__main__':
+    CameraApp().run()
